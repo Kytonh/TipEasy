33f61a7d1625c7af249d2e031258ccd27adfa087
何恺君 <kytonhe@macbook-8.local>
2018-12-23 00:46:48 -0500

    Initial Commit

---
+++ tipEasy2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipEasy2
+//
+//  Created by 何恺君 on 12/23/18.
+//  Copyright © 2018 Kaijun He. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ tipEasy2Tests/tipEasy2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  tipEasy2Tests.swift
+//  tipEasy2Tests
+//
+//  Created by 何恺君 on 12/23/18.
+//  Copyright © 2018 Kaijun He. All rights reserved.
+//
+
+import XCTest
+@testable import tipEasy2
+
+class tipEasy2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
