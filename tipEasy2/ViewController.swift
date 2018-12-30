0ec9f1c46c1e829d72c66e60fe87eb33281d1ae8
何恺君 <kytonhe@MacBook-8.local>
2018-12-30 01:33:54 -0500

    add .gitattributes and .gitignore and README

--- tipEasy2/ViewController.swift
+++ tipEasy2/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var percentControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBAction func OnTap(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
+    @IBAction func Calculation(_ sender: AnyObject) {
+        let percent = [0.15,0.18,0.20]
+        let bill=Double(billField.text!) ?? 0
+        let tip=bill*percent[percentControl.selectedSegmentIndex]
+        let total=bill+tip
+        
+        tipLabel.text = String(format:"$%.2f",tip)
+        totalLabel.text = String(format:"$%.2f",total)
+    }
+    
+    
 }
 
